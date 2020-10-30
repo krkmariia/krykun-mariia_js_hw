609a2d86be01c24b900426d6da3b6cbf39ca92b4
krkmariia <61343459+krkmariia@users.noreply.github.com>
2020-10-30 15:45:39 +0200

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,18 @@
+const findLongestWord = function (string) {
+    let str = string.split(" ");
+    let longest = '';
+    let word = null;
+
+    for (word of str) {
+    if (longest.length < word.length){
+        longest = word;
+    }
+    }
+    return longest;
+  };
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
